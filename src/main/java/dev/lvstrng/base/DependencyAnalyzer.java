6fc9497e56ed3727d4eb658749b2c315103bf2ee
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-07-19 17:29:02 +0530

    fix: analyze java modules and the input jar

--- src/main/java/dev/lvstrng/base/DependencyAnalyzer.java
+++ src/main/java/dev/lvstrng/base/DependencyAnalyzer.java
@@ -5,17 +5,23 @@ import org.objectweb.asm.tree.ClassNode;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.zip.ZipFile;
 
 /**
  * @author SpigotRCE
- * Adds all library classes to a map to computer frams in {@link dev.lvstrng.base.utils.CustomClassWriter}
+ * Adds all library classes to a map to compute frames in {@link dev.lvstrng.base.utils.CustomClassWriter}
  */
 public class DependencyAnalyzer {
-    public static HashMap<String, ClassNode> libraryClasses = new HashMap<>();
+    public static final HashMap<String, ClassNode> libraryClasses = new HashMap<>();
 
     public static void analyzeDependencies(File dependenciesDirectory) {
+        analyzeJdkModules();
+        System.out.println("Analyzing dependencies in: " + dependenciesDirectory.getAbsolutePath());
         if (!dependenciesDirectory.isDirectory()) {
             System.out.println("Provided path is not a directory: " + dependenciesDirectory.getAbsolutePath());
             return;
@@ -27,28 +33,72 @@ public class DependencyAnalyzer {
             return;
         }
 
+        System.out.println("Found " + files.length + " dependencies.");
+
         for (File file : files) {
             if (file.isFile() && file.getName().endsWith(".jar")) {
-                try {
-                    try (var zipFile = new ZipFile(file)) {
-                        for(var entry : zipFile.stream().toList()) {
-                            try (var in = zipFile.getInputStream(entry)) {
-                                var name = entry.getName();
+                System.out.println("Analyzing jar: " + file.getName());
+                try (ZipFile zipFile = new ZipFile(file)) {
+                    for (var entry : zipFile.stream().toList()) {
+                        try (InputStream in = zipFile.getInputStream(entry)) {
+                            var name = entry.getName();
 
-                                if(name.endsWith(".class")) {
-                                    ClassReader reader = new ClassReader(in);
-                                    ClassNode node = new ClassNode();
-                                    reader.accept(node, 0);
-                                    libraryClasses.put(node.name, node);
-                                }
+                            if (name.endsWith(".class")) {
+                                ClassReader reader = new ClassReader(in);
+                                ClassNode node = new ClassNode();
+                                reader.accept(node, 0);
+                                libraryClasses.put(node.name, node);
                             }
+                        } catch (IOException ignored) {
                         }
-                    } catch (IOException _) {}
+                    }
                 } catch (Exception e) {
                     System.err.println("Failed to analyze jar: " + file.getName());
                     e.printStackTrace();
                 }
             }
+        }
+
+        System.out.println("Analyzed " + libraryClasses.size() + " classes from dependencies.");
+    }
+
+    public static void analyzeJdkModules() {
+        System.out.println("Analyzing all JDK modules via jrt:/");
+
+        try {
+            FileSystem jrtFs;
+            try {
+                jrtFs = FileSystems.getFileSystem(URI.create("jrt:/"));
+            } catch (FileSystemNotFoundException e) {
+                jrtFs = FileSystems.newFileSystem(URI.create("jrt:/"), Collections.emptyMap());
+            }
+
+            Path modulesPath = jrtFs.getPath("/modules");
+
+            try (var modules = Files.list(modulesPath)) {
+                for (Path modulePath : modules.toList()) {
+                    String moduleName = modulePath.getFileName().toString();
+                    System.out.println("Scanning module: " + moduleName);
+
+                    Files.walk(modulePath)
+                            .filter(p -> p.toString().endsWith(".class"))
+                            .forEach(p -> {
+                                try (InputStream in = Files.newInputStream(p)) {
+                                    ClassReader reader = new ClassReader(in);
+                                    ClassNode node = new ClassNode();
+                                    reader.accept(node, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
+                                    libraryClasses.put(node.name, node);
+                                } catch (IOException ignored) {
+                                }
+                            });
+                }
+            }
+
+            System.out.println("Finished analyzing JDK modules. Total classes: " + libraryClasses.size());
+
+        } catch (IOException e) {
+            System.err.println("Failed to analyze JDK modules.");
+            e.printStackTrace();
         }
     }
 }
